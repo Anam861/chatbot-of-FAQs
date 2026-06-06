7e3bd60bf7db450d18103bd6462fd40f8ef1553a
Anam861 <tahirmehar870@gmail.com>
2026-06-06 20:38:31 +0500

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,13 @@
+Const faqs={
+    "what is html":
+       "html is used to create web pages:",
+
+    "what is css":
+        "Css is used for styling web pages.",
+
+    "what is javascript":
+         "javascript makes websites interactive.",
+
+     "what is react":
+          "React is a javascript library."
+}
